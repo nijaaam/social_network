98d55967942ac09f6f58e38fe640f28e7aa58da3
Bagus Maulana <bmaulana273@gmail.com>
2017-02-23 14:45:37 +0000

    Fixed login page

--- login.php
+++ login.php
@@ -4,10 +4,11 @@
 	if (isset($_POST['submit1'])) {
 		$username = mysqli_real_escape_string($conn, $_POST['username']);
 		$password = md5(mysqli_real_escape_string($conn, $_POST['password']));
-		$query = "SELECT * FROM users WHERE userID = $username AND password = $password";
+		$query = "SELECT * FROM `users` WHERE username = '$username' AND password = '$password'";
 		$result = mysqli_query($conn,$query);
-		if ($result && mysql_num_rows($result) == 1){
+		if ($result && mysqli_num_rows($result) == 1){
 			header("location: home.php");
+			// TODO need to store who the logged in user is (and make home.php)
 			exit();
 		} else {
 			header("location: index.php");
