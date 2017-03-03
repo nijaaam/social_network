31927b5ab3256d22cdb99bc72b20406f34a02edc
Shivam Dhall <dhallshivam@gmail.com>
2017-03-03 17:58:14 +0000

    refactor code in register.php

---
+++ validation_functions.php
@@ -0,0 +1,54 @@
+<?php
+	
+	// clean user inputs 
+	function clean_data($name){
+		$value = trim($_POST[$name]);
+		$value = strip_tags($value);
+		$value = htmlspecialchars($value);
+		return $value;
+	}
+
+
+	function validate($name, $value, $minsize, $format=false){
+		global $error;
+		if(empty($value)){
+			$error = true;
+			return "Please enter your {$name}";
+		}
+		else if(strlen($value) < $minsize){
+			$error = true;
+			return ucfirst("{$name} must have at least {$minsize} characters");
+		}
+		else if($format == true && !preg_match("/^[a-zA-Z ]+$/", $value)){
+			$error = true;
+			return "Field must contain alphabet or space characters";
+		}
+	}
+
+	function validate_select($name, $value){
+		global $error;
+		if(empty($value)){
+			$error = true;
+			return "Please select a {$name}";
+		}
+	}
+
+	function validate_email($value){
+		global $error;
+		if (!filter_var($value, FILTER_VALIDATE_EMAIL)){
+			$error = true;
+			return "Please enter a valid email address.";
+		} else {
+			// check email exist or not
+			$query = "SELECT email FROM users WHERE email='$value'";
+			$result = mysql_query($query);
+			$count = mysql_num_rows($result);
+			if($count!=0){
+				$error = true;
+				return "Provided Email is already in use.";
+			}
+		}
+	}
+
+?>
+
