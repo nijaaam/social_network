c9f9d2143c23a9821afb05e6e18bdad5855a0836
Bagus Maulana <bmaulana273@gmail.com>
2017-03-10 01:42:54 +0000

    foreign keys

--- droptables.sql
+++ droptables.sql
@@ -1,13 +1,12 @@
 DROP TABLE IF EXISTS blogposts;
 DROP TABLE IF EXISTS circlememberships;
-DROP TABLE IF EXISTS circles;
-DROP TABLE IF EXISTS generalsettings;
 DROP TABLE IF EXISTS messagecircles;
+DROP TABLE IF EXISTS circles;
 DROP TABLE IF EXISTS personalinfo;
-DROP TABLE IF EXISTS photocollections;
 DROP TABLE IF EXISTS photocomments;
 DROP TABLE IF EXISTS photolikes;
 DROP TABLE IF EXISTS photos;
+DROP TABLE IF EXISTS photocollections;
 DROP TABLE IF EXISTS relationships;
 DROP TABLE IF EXISTS securitysettings;
 DROP TABLE IF EXISTS users;
